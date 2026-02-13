47b2bfd631b223dff7eaa9432faad8c5441bd781
Shashi <shashikanth0312@gmail.com>
2026-02-13 21:14:37 +0530

    adding

---
+++ terraforn-vpc-test/variables.tf
@@ -0,0 +1,15 @@
+variable "project_name" {
+  default = "expense"
+}
+
+variable "common_tags" {
+  default = {
+    Project = "expense"
+    Pnvironment = "dev"
+    Terraform = true
+  }
+}
+
+variable "public_subnet_cidrs" {
+  default = ["10.0.1.0/24"]
+}
