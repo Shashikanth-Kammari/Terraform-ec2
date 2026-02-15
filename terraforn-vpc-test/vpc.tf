21eb41bd10f061e44662fe59d6d3b9347ea6f998
Shashi <shashikanth0312@gmail.com>
2026-02-15 11:00:23 +0530

    adding

--- terraforn-vpc-test/vpc.tf
+++ terraforn-vpc-test/vpc.tf
@@ -3,4 +3,5 @@ module "vpc" {
     Project_name = var.Project_name
     common_tags = var.common_tags
     public_subnet_cidrs = var.public_subnet_cidrs
+    private_subnet_cidrs = var.public_subnet_cidrs
 }
