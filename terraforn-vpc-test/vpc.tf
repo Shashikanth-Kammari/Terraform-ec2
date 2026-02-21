84cab1c8551aa7f418f6e90d030bcdceca88b010
Shashi <shashikanth0312@gmail.com>
2026-02-21 14:01:22 +0530

    adding

--- terraforn-vpc-test/vpc.tf
+++ terraforn-vpc-test/vpc.tf
@@ -1,6 +1,6 @@
 module "vpc" {
     # source = "./terraform-aws-vpc"
-    source = "git::https://github.com/Shashikanth-Kammari/terraform-aws-vpc.git"
+    source = "git::https://github.com/Shashikanth-Kammari/terraform-aws-vpc.git?ref=main"
     Project_name = var.Project_name
     common_tags = var.common_tags
     public_subnet_cidrs = var.public_subnet_cidrs
